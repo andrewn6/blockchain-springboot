2d45dcc06892cc5b5d53a221b13706afec8342c9
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-04-02 20:57:20 -0400

    wip

--- src/main/java/agent/AgentManager.java
+++ src/main/java/agent/AgentManager.java
@@ -1,8 +1,12 @@
 package agent;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class AgentManager  {
 
+    private List<Agent> agents = new ArrayList<>();
+    private static final Block root = new Block(0, "ROOT_HASH", "ROOT")
 }
