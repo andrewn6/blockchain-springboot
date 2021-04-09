1c0d6f3e56c8fd4f6ff91978131dff6a591b19a2
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-04-08 21:03:31 -0400

    Finish agent controller. - Remove all agents - Create block - remove 1 singular agent

--- src/main/java/web/AgentController.java
+++ src/main/java/web/AgentController.java
@@ -1,7 +1,11 @@
 package web;
 
-
-import org.springframework.web.bind.annotation.
+import agent.Agent;
+import agent.AgentManager;
+import agent.Block;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
@@ -9,10 +13,41 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 // Main controller class for all agent methods
-
 @RestController
 @RequestMapping(path="/agent")
-
 public class AgentController {
+
+    private static final AgentManager agentManager = new AgentManager();
+
+    @RequestMapping(method=GET)
+    public Agent getAgent(@RequestParam("name") String name) {
+        return agentManager.getAgent(name);
+    }
+
+    @RequestMapping(method=DELETE)
+    public void removeAgent(@RequestParam("name") String name) {
+        agentManager.removeAgent(name);
+    }
+
+    @RequestMapping(method=POST, params = {"name", "port"})
+    public Agent addAgent(@RequestParam("name") String name, @RequestParam("port") int port) {
+        return agentManager.addAgent(name, port);
+    }
+
+    @RequestMapping(path="all", method=GET)
+    public List<Agent> getAllAgents() {
+        return agentManager.getAllAgents();
+    }
+
+    @RequestMapping(path="all", method=DELETE)
+    public void removeAllAgents() {
+        agentManager.removeAllAgents();
+    }
+
+    @RequestMapping(method=POST, path="mine")
+    public Block createBlock(@RequestParam(value="agent") final String name) {
+        return agentManager.createBlock(name);
+    }
+
 
 }
